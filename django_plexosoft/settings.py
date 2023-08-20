0f09f5c6eeebe17d597ab3f335e063e5eb8d476b
Frank Reinaldo Arellano Andrade <plexonft@gmail.com>
2023-08-20 20:19:54 +0000

    ready for heroku

--- django_plexosoft/settings.py
+++ django_plexosoft/settings.py
@@ -25,11 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
-
-production = False
+IN_DEVELOPMENT = os.environ.get('IN_DEVELOPMENT', False)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = production
+DEBUG = IN_DEVELOPMENT
 
 ALLOWED_HOSTS = ['8000-plexoio-py-om3gwfq21br.ws-eu104.gitpod.io']
 
@@ -107,7 +106,7 @@ WSGI_APPLICATION = 'django_plexosoft.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if production:
+if IN_DEVELOPMENT:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
