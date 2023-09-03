b990b6c251a8b79579c9f48b4e35f936ea3b64d6
Frank Reinaldo Arellano Andrade <plexonft@gmail.com>
2023-09-03 11:19:28 +0000

    feat: context processor for EmailAddress model complete

--- homepage/custom_context_processors.py
+++ homepage/custom_context_processors.py
@@ -1,13 +1,14 @@
-# custom_context_processors.py
+# Global context processor for EmailAddress model
 
 from allauth.account.models import EmailAddress
 
 
 def get_verified_email(request):
-    try:
-        email_object = EmailAddress.objects.get(user=request.user)
-        if email_object.verified:
-            return {'get_verified_email': email_object.email}
-    except EmailAddress.DoesNotExist:
-        pass
+    if request.user.is_authenticated:
+        try:
+            email_object = EmailAddress.objects.get(user=request.user)
+            if email_object.verified:
+                return {'get_verified_email': email_object}
+        except EmailAddress.DoesNotExist:
+            pass
     return {'get_verified_email': None}
