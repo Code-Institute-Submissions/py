f104153f296965da22b8678d5689a0fcf7948a87
Frank Reinaldo Arellano Andrade <plexonft@gmail.com>
2023-09-14 20:43:41 +0000

    feat: develop sorting on page javascript, pending improvement

---
+++ docs/developer-snack/naming_convention.md
@@ -0,0 +1,31 @@
+## Variable Naming Conventions in Different Languages
+
+This documentation outlines common variable naming conventions used in JavaScript, HTML, and Python. These conventions help maintain consistency and readability in your code. Here's a summary of each convention along with its typical use cases:
+
+### JavaScript Variable Naming Conventions
+
+| Convention    | Example          | Use Case Scenario                                       |
+|---------------|------------------|---------------------------------------------------------|
+| Camel Case    | `myVariableName` | Common for variables in JavaScript functions and methods. |
+| Pascal Case   | `MyVariableName` | Typically used for constructor functions and classes in JavaScript. |
+| Snake Case    | `my_variable_name` | Less common but sometimes used for variables that simulate snake_case. |
+| Kebab Case    | `my-variable-name` | Often used for naming in HTML and CSS, less common for JavaScript variables. |
+
+### HTML Attribute Naming Conventions
+
+| Convention        | Example                        | Use Case Scenario                                       |
+|-------------------|--------------------------------|---------------------------------------------------------|
+| Id                | `<div id="my-id"></div>`       | Used for uniquely identifying HTML elements for styling or JavaScript manipulation. |
+| Class             | `<div class="my-class"></div>` | Used to apply common styling or JavaScript functionality to multiple elements. |
+| Data Attribute    | `<div data-info="my-data"></div>` | Used to store custom data associated with an HTML element for JavaScript access. |
+
+### Python Variable Naming Conventions
+
+| Convention        | Example          | Use Case Scenario                                       |
+|-------------------|------------------|---------------------------------------------------------|
+| Snake Case        | `my_variable_name` | Standard naming convention for Python variables and functions. |
+| Camel Case        | `myVariableName` | Occasionally used for variable names in Python, especially in class attributes. |
+| Pascal Case       | `MyVariableName` | Rarely used in Python, mainly reserved for class names or constants. |
+| Uppercase Snake Case | `MY_CONSTANT`  | Common for naming constants in Python. |
+
+Choosing the appropriate naming convention is essential for code maintainability and collaboration. Consistency within a project and adherence to the conventions of the respective language are key factors in writing clean and understandable code.
--- static/frontend/js/script.js
+++ static/frontend/js/script.js
@@ -1,18 +1,77 @@
 $(document).ready(function () {
+    // BUTTON to scroll up
+    $('.btn-scroll').click(function (e) {
+        window.scrollTo(0, 0)
+    });
+
+    // SORTING on a dedicated page
     $('#sort-selector').on('change', function () {
-        var selectedValue = $(this).val();
+        let selectedValue = $(this).val();
 
         if (selectedValue != 'reset') {
-            var parts = selectedValue.split('_');
-            var domain = parts[0];
-            var sort = parts[1];
-            var direction = parts[2];
+            let parts = selectedValue.split('_');
+            let domain = parts[0];
+            let sort = parts[1];
+            let direction = parts[2];
 
             // URL Construction
-            var url = domain + "?sort=" + sort + "&direction=" + direction;
+            let url = domain + "?sort=" + sort + "&direction=" + direction;
 
             // Redirection to the constructed URL
             window.location.href = url;
         }
     });
+
+    // SORTING on the same page
+    function rearrangeItems() {
+        let selectedOption = $('#sort-selector-product').val();
+        let itemElements = [];
+
+        $('.item').each(function () {
+            let $item = $(this);
+            let priceContent = parseFloat($item.find('.price').html());
+            let likesCount = parseFloat($item.find('.likes').html());
+            let transactionsCount = parseFloat($item.find('.transactions').html());
+            let titleContent = $item.find('.title').html();
+
+            itemElements.push({
+                element: $item,
+                price: priceContent,
+                likes: likesCount,
+                transactions: transactionsCount,
+                title: titleContent
+            });
+        });
+
+        // Sorting arrow functions as per selected option
+        const sortingFunctions = {
+            'price_asc': (a, b) => a.price - b.price,
+            'price_desc': (a, b) => b.price - a.price,
+
+            'likes_asc': (a, b) => a.likes - b.likes,
+            'likes_desc': (a, b) => b.likes - a.likes,
+
+            'transactions_asc': (a, b) => a.transactions - b.transactions,
+            'transactions_desc': (a, b) => b.transactions - a.transactions,
+
+            'title_asc': (a, b) => a.title.localeCompare(b.title),
+            'title_desc': (a, b) => b.title.localeCompare(a.title),
+        };
+
+        if (selectedOption in sortingFunctions) {
+            itemElements.sort(sortingFunctions[selectedOption]);
+
+            for (let i = 0; i < itemElements.length; i++) {
+                $('#item-container').append(itemElements[i].element);
+            }
+
+        } else {
+            let currentUrl = new URL(window.location);
+            window.location.replace(currentUrl);
+        }
+    }
+
+    // CALL the rearrangeItems function when the selector changes
+    $('#sort-selector-product').on('change', rearrangeItems);
+
 });
