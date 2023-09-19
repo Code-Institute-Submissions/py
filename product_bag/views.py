9b17277113b4303f7c6d60ff34b25b18b7c5f7e1
Frank Reinaldo Arellano Andrade <plexonft@gmail.com>
2023-09-19 13:17:42 +0000

    refactor: error handling and structure complete for the cart logic

--- product_bag/views.py
+++ product_bag/views.py
@@ -1,23 +1,25 @@
+# Django Imports
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.http import HttpResponseServerError
 from django.views.generic import View, ListView
 from django.contrib import messages
-from django.http import HttpResponse
 # Python
 import logging
-
+# Local Imports
 from product_service.views import Product
 
 logger = logging.getLogger(__name__)
 
-# Cart Functionality
+# PRODUCT in Cart
+
+# CREATE & READ Product instances in the cart
 
 
 class ProductAddToCartMixin:
     def add_to_cart(self, product_id):
+        """ Add-to-cart method in this module for scalability"""
         try:
             product = Product.objects.filter(status=2).get(id=product_id)
-            # "add to cart" logic here
-
             return product
         except Product.DoesNotExist:
             return False
@@ -30,50 +32,50 @@ class ProductAddToCartView(View, ProductAddToCartMixin):
         """ Get POST parameters and create session.
         Remember to use "if product_id: request.session.clear()" to clear
         the session when developing."""
+        try:
+            quantity = int(request.POST.get('quantity'))
+            redirect_url = request.POST.get('redirect_url')
+            product_bag = request.session.get('product_bag', {})
+            product = self.add_to_cart(product_id)
+            title = product.title
 
-        quantity = int(request.POST.get('quantity'))
-        redirect_url = request.POST.get('redirect_url')
-        product_bag = request.session.get('product_bag', {})
-        product = self.add_to_cart(product_id)
-        title = product.title
+            if product_id in list(product_bag.keys()):
+                product_bag[product_id]['quantity'] += quantity
+            else:
+                product_bag[product_id] = {
+                    'title': title,
+                    'quantity': quantity,
+                }
 
-        if product_id in list(product_bag.keys()):
-            product_bag[product_id]['quantity'] += quantity
-        else:
-            product_bag[product_id] = {
-                'title': title,
-                'quantity': quantity,
-            }
+            request.session['product_bag'] = product_bag
 
-        request.session['product_bag'] = product_bag
+            messages.success(
+                request, f'''You have added
+                <strong>{product_bag[product_id]['quantity']}</strong> products
+                for <strong>{product_bag[product_id]['title']}</strong>
+                in the cart!'''
+            )
 
-        messages.info(
-            request, f'''You have
-            <strong>{product_bag[product_id]['quantity']}</strong> products
-            for <strong>{product_bag[product_id]['title']}</strong>
-            in the cart!'''
-        )
-
-        return redirect(redirect_url)
+            return redirect(redirect_url)
+        except KeyError:
+            messages.error(
+                request, '''Bag, product ID or its data
+                not found in your cart!''')
+            return redirect(reverse('bag_page'))
+        except Exception as e:
+            logger.error(f"An error occurred: {str(e)}")
+            return HttpResponseServerError()
 
 
 class ProductShoppingCartView(ListView, ProductAddToCartMixin):
     model = Product
     template_name = 'bag/bag.html'
-    context_object_name = 'products'
 
-    def get_queryset(self):
-        """ Return product instances ordered by creation date."""
-        return Product.objects.order_by('-created_on')
-
-    def post(self, request, product_id):
-        print('placeholder')
-
-# UPDATE Product Cart
+# UPDATE Product instances in the cart
 
 
 class UpdateProductCartView(View):
-    ''' Update product instance in the cart'''
+    ''' Update Product instance in the cart'''
 
     def post(self, request, product_id):
         """ Get POST parameters and update session."""
@@ -97,7 +99,7 @@ class UpdateProductCartView(View):
 
             request.session['product_bag'] = product_bag
 
-            messages.info(
+            messages.success(
                 request, f'''You have
                 <strong>{product_bag[product_id]['quantity']}</strong> products
                 for <strong>{product_bag[product_id]['title']}</strong>
@@ -114,11 +116,11 @@ class UpdateProductCartView(View):
             logger.error(f"An error occurred: {str(e)}")
             return HttpResponseServerError()
 
-# DELETE Product Cart
+# DELETE Product instances in the cart
 
 
 class DeleteProductCartView(View):
-    ''' Delete product instance from cart '''
+    ''' Delete Product instance from cart '''
 
     def post(self, request, product_id):
         try:
