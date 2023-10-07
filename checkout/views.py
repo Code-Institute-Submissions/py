f5928742a379bb47890b727c0eee82abd3ba2bb1
Frank Reinaldo Arellano Andrade <plexonft@gmail.com>
2023-10-07 13:56:07 +0000

    refactor: clean code again better comments to the checkout view

--- checkout/views.py
+++ checkout/views.py
@@ -10,9 +10,28 @@ from .models import Order
 
 
 class Checkout(TemplateView):
+    """
+    Handles the checkout page, where the user sees the final order details.
+
+    Attributes:
+        template_name (str): The name of the template to be rendered.
+
+    Methods:
+        get(): Handles GET requests for the checkout page.
+            - Checks user authentication.
+            - Redirects to the bag page if the bag is empty.
+
+        get_context_data(): Populates additional context data for rendering
+        the template.
+            - Adds the 'order_form' to the context.
+    """
     template_name = 'checkout/checkout.html'
 
     def get(self, request, *args, **kwargs):
+        """
+        Handles GET requests.
+        Checks if the user is authenticated and if the bag is empty.
+        """
 
         if not request.user.is_authenticated:
             messages.warning(
@@ -27,6 +46,9 @@ class Checkout(TemplateView):
         return super().get(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
+        """
+        Populates additional context for rendering the template.
+        """
         context = super().get_context_data(**kwargs)
         context['order_form'] = OrderForm(request=self.request)
         return context
