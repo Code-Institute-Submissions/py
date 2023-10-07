7961625bea736dbd428f7004daeb8b7d104852e6
Frank Reinaldo Arellano Andrade <plexonft@gmail.com>
2023-10-07 13:42:09 +0000

    refactor: clean webhooks.py code, new comments and import strcture

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -1,21 +1,55 @@
+# Django standard library imports
 from django.views import View
+from django.http import HttpResponse
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
+
+# Third-party library imports
+import os
 import stripe
+
+# Application-specific imports
 from .webhook_handler import StripeWebhookHandler
 from django.conf import settings
-from django.http import HttpResponse
-import os
 
+# Initialize Stripe API
 stripe.api_key = os.environ.get('STRIPE_SECRET_KEY')
 endpoint_secret = os.environ.get('STRIPE_ENDPOINT_SECRET_KEY')
 
 
 @method_decorator(csrf_exempt, name='dispatch')
 class StripeWebhookView(View):
+    """
+    View to handle incoming Stripe Webhooks.
+
+    This view is CSRF exempt and only allows POST requests to handle
+    Stripe webhook events. The events are then passed to the appropriate
+    event handlers for further processing.
+
+    Attributes:
+        http_method_names (list): List of allowed HTTP methods.
+        Here, only POST is allowed.
+    """
     http_method_names = ['post']  # Allow only POST method
 
     def post(self, request, *args, **kwargs):
+        """
+        Handle incoming POST request containing Stripe webhook data.
+
+        The method performs the following steps:
+        1. Parse and verify the incoming payload and signature from Stripe.
+        2. Identify the type of event (e.g., payment succeeded, payment failed)
+        3. Dispatch the event to the appropriate handler function
+        for further action.
+
+        Args:
+            request (HttpRequest): The request object containing
+            metadata and payload.
+
+        Returns:
+            HttpResponse: An HTTP response indicating the status
+            of the operation.
+        """
         payload = request.body
         sig_header = request.META['HTTP_STRIPE_SIGNATURE']
         event = None
