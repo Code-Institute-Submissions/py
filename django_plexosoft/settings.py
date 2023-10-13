cb79fa54b253a059ecbcf057038bb0d7695a048d
Frank Reinaldo Arellano Andrade <plexonft@gmail.com>
2023-10-13 22:31:04 +0000

    feat: email verification and order confirmation email complete

--- django_plexosoft/settings.py
+++ django_plexosoft/settings.py
@@ -118,8 +118,6 @@ if DEBUG:
 else:
     SITE_ID = 2  # ID for production domain
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
@@ -254,3 +252,15 @@ CACHES = {
 # Order Confirmation Email
 
 DEFAULT_FROM_EMAIL = 'plexosoft@plexosoft.com'
+
+if 'IN_DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
